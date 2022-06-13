bf11d235241397ccd46a2ba9d9ef78b7620a2f13
GongT <admin@gongt.me>
2022-06-14 03:58:30 +0800

    Init

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@atlaskit/button';
+
+export class TestComp extends React.PureComponent<{}, any, any> {
+	render() {
+		return (
+			<Button>Hello world!</Button>
+		);
+	}
+}
